a6e2e2d34febc118b8d5e46d9db07e9911cc8a76
Bruno Lucho Macedo <bmacedo@weg.net>
2021-06-14 23:06:05 -0300

    Os arquivos não estavam adicionados

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Olá")
+
+const calculate = document.getElementById('calculate')
+
+function bmi() {
+    const height = document.getElementById('height').value
+    const weight = document.getElementById('weight').value
+    const result = document.getElementById('result')
+
+    if(height !== '' && weight !== '') {
+        const bmiValue = (weight / (height * height)).toFixed(1)
+        result.textContent = bmiValue
+    } else {
+        result.textContent = 'Please enter all fields above...'
+    }
+}
+
+calculate.addEventListener('click', bmi)
